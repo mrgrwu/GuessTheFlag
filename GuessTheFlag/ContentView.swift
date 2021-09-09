d95e5b1eb2a24a84060802112fdb4a055b83c8fa
Greg Wu <mrgrwu@gmail.com>
2021-09-08 19:41:42 -0700

    Create custom FlagImage view to render flag image buttons.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -26,11 +26,7 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                        FlagImage(imageName: self.countries[number])
                     }
                 }
                 Text("Score: \(score)").foregroundColor(.white).font(.largeTitle)
---
+++ GuessTheFlag/FlagImage.swift
@@ -0,0 +1,20 @@
+//
+//  FlagImage.swift
+//  GuessTheFlag
+//
+//  Created by Greg Wu on 9/8/21.
+//
+
+import SwiftUI
+
+struct FlagImage: View {
+    var imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            .shadow(color: .black, radius: 2)
+    }
+}
